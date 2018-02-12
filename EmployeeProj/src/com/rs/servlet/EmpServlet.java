5eb4b8a632530e177e1c4ba54731b720b99e4657
Ramesh <rameshpanthangi08@gmail.com>
2018-02-12 15:21:50 +0530

    Emp Fun added

--- EmployeeProj/src/com/rs/servlet/EmpServlet.java
+++ EmployeeProj/src/com/rs/servlet/EmpServlet.java
@@ -1,6 +1,7 @@
 package com.rs.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 public class EmpServlet extends HttpServlet{
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		
+		PrintWriter pw=null;
+		res.setContentType("text/html");
 	}
 	public void doPost(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException{
 		doGet(req,res);
