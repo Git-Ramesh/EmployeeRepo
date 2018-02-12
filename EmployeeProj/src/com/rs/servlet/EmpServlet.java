8ec9fdbf2f9eae13de5d454cb43979e633bbfd04
Ramesh <rameshpanthangi08@gmail.com>
2018-02-12 14:56:43 +0530

    post method added

--- EmployeeProj/src/com/rs/servlet/EmpServlet.java
+++ EmployeeProj/src/com/rs/servlet/EmpServlet.java
@@ -9,8 +9,10 @@ import javax.servlet.http.HttpServletResponse;
 
 public class EmpServlet extends HttpServlet{
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+	}
+	public void doPost(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException{
+		doGet(req,res);
 	}
 }
